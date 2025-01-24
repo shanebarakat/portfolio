119d2d5a4b3092c1632d3ab9e9c52ce997e7307f
Shane Barakat <srbaraka@uwaterloo.ca>
2025-01-23 22:32:53 -0500

    Added Logos for Icons

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,18 +1,47 @@
 import PlaceholderImage from './assets/Placeholder (1).png';
+import icon from './assets/20308214691623491653.svg';
+import icon2 from "./assets/13880855881556105710.svg";
+import icon3 from "./assets/17952689171595156226.svg"
+import icon4 from "./assets/12090117491553664904.svg"
 
 const App: React.FC = () => {
   return (
     <div className="bg-black text-white min-h-screen w-screen flex flex-col justify-start items-center pt-16 px-0 scrollbar-y-hide">
-      <h1 className="text-6xl font-bold mb-8">Shane Barakat</h1>
+      <h1 className="text-6xl font-bold mb-8 font-outfit">Shane Barakat</h1>
       <h2 className="text-xl font-bold mb-8 max-w-[500px] text-center">
         Management Engineering Student at UWaterloo Exploring Software, Product, and Data Roles
       </h2>
       <div className="flex gap-4">
-        <button className="px-4 py-2 bg-gray-700 rounded hover:bg-gray-600">A</button>
-        <button className="px-4 py-2 bg-gray-700 rounded hover:bg-gray-600">B</button>
-        <button className="px-4 py-2 bg-gray-700 rounded hover:bg-gray-600">C</button>
-        <button className="px-4 py-2 bg-gray-700 rounded hover:bg-gray-600">D</button>
-      </div>
+  <a href="https://github.com/shanebarakat" target="_blank" rel="noreferrer">
+  <img
+    src={icon4}
+    className="w-12 h-12 transition-transform duration-300 hover:scale-110 drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
+    alt="Icon 1"
+  />
+  </a>
+  <a href="https://www.linkedin.com/in/shane-barakat/" target="_blank" rel="noreferrer">
+  <img
+    src={icon2}
+    className="w-11 h-12 transition-transform duration-300 hover:scale-110 drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
+    alt="Icon 2"
+  />
+  </a>
+  <a href= "mailto:srbaraka@uwaterloo.ca" target="_blank" rel="noreferrer">
+  <img
+    src={icon3}
+    className="w-12 h-12 transition-transform duration-300 hover:scale-110 drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
+    alt="Icon 3"
+  />
+  </a>
+  
+  <img
+    src={icon}
+    className="w-12 h-12 transition-transform duration-300 hover:scale-110 drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
+    alt="Icon 4"
+  />
+</div>
+
+
       <div className="flex flex-col gap-8 mt-8 justify-center w-screen px-4">
   {/* Row 1 */}
   <div className="flex flex-col md:flex-row gap-4 justify-center">
