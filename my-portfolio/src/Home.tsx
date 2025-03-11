921e0ec0b9d0ca9f03afc1aa9425d09ff61ae492
Shane Barakat <srbaraka@uwaterloo.ca>
2025-03-10 21:40:39 -0500

    FIxed Resume Link

--- my-portfolio/src/Home.tsx
+++ my-portfolio/src/Home.tsx
@@ -49,10 +49,10 @@ const App: React.FC = () => {
   <a href= "mailto:srbaraka@uwaterloo.ca" target="_blank" rel="noreferrer">
   <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="grey" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-mail transition-colors duration-300 hover:stroke-white"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>
   </a>
-  <a href= "https://drive.google.com/file/d/1T4Q41Rggt_ytfP37bXVqXJ_MWQlDI-yI/view?usp=sharing" target="_blank" rel="noreferrer">
+  <a href= "https://drive.google.com/file/d/13KQR4zeVtuHj4WSdzSi7eHrIvE__FRI3/view?usp=sharing" target="_blank" rel="noreferrer">
   <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="grey" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-file-user transition-colors duration-300 hover:stroke-white"><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M15 18a3 3 0 1 0-6 0"/><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7z"/><circle cx="12" cy="13" r="2"/></svg>
   </a>
-</div>
+</div> 
 
 
       <div className="flex  flex-col gap-8 mt-8 justify-center max-w-screen px-4 box-border">
