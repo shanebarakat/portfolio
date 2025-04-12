58a5a22d7f16380b3ff4d9c059717ffce7e073b2
Shane Barakat <srbaraka@uwaterloo.ca>
2025-04-12 14:52:50 -0500

    test

--- my-portfolio/src/Home.tsx
+++ my-portfolio/src/Home.tsx
@@ -39,7 +39,7 @@ const App: React.FC = () => {
       <h2 className="text-5px sm:text-xl font-medium font-noto mb-6 max-w-[500px] text-center ">
         Management Engineering Student at UWaterloo Exploring Software, Product, and Data Roles
       </h2>
-      <div className="flex gap-4 ">
+      <div className="flex gap-4  ">
   <a href="https://github.com/shanebarakat" target="_blank" rel="noreferrer">
   <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="grey" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-github transition-colors duration-300 hover:stroke-white"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
   </a>
